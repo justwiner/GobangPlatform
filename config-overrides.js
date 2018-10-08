66b0120600b4655f5cb1a6333b3384b4d338fc22
justwiner <392110917@qq.com>
2018-10-08 13:48:20 +0800

    定制主题

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 module.exports = function override(config, env) {
     // do stuff with the webpack config...
-    config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+    config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: true }], config);
     return config;
   };
