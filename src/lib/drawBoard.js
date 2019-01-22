44bac66d24db6f25c002b424fbca5045b00a69ca
justwiner <392110917@qq.com>
2019-01-22 19:20:02 +0800

    完成棋盘绘制逻辑代码的注释

--- src/lib/drawBoard.js
+++ src/lib/drawBoard.js
@@ -82,37 +82,51 @@ function drawBoard(context, borderWidth, border, spec, width, chessRecords) {
         })
     });
 }
-// 绘制线条
+// 封装->绘制线条
 function drawLine(context, options) {
+    // 绘制起点
     context.moveTo(options.start.x, options.start.y);
+    // 绘制终点
     context.lineTo(options.end.x, options.end.y);
+    // 给线条上色
     if (options.strokeColor)
         context.strokeStyle = options.strokeColor;
+    // 以笔触(类似笔一样，画路径)的方式绘制， fill()方法为填充
     context.stroke();
 }
-// 绘制棋子
+// 封装->绘制棋子
 function drawCircle(context, options) {
+    // 清除之前所有的画笔设置
     context.beginPath();
+    // arc(圆心x坐标，圆心y坐标，圆的半径，起始角，结束角，true=逆时针 | false=顺时针)
     context.arc(options.point.x, options.point.y, options.width, 0, 2 * Math.PI, true);
     let grd = null;
     if (options.type !== undefined && options.type !== null) {
+        // 绘制棋子外侧的渐变色
         grd = context.createRadialGradient(options.point.x, options.point.y, options.width, options.point.x, options.point.y, 0);//设置渐变
     }
     if (options.type === 0) {
+        // 棋子外侧渐变色的边框的 宽度与颜色范围
         grd.addColorStop(0,'#0A0A0A');
         grd.addColorStop(1,'#636766');
     } else if (options.type === 1) {
+        // 棋子外侧渐变色的边框的 宽度与颜色范围
         grd.addColorStop(0,'#D1D1D1');
         grd.addColorStop(1,'#F9F9F9');
     }
     if (options.fillStyle)
+        // 设置填充颜色
         context.fillStyle = options.fillStyle;
     else
+        // 设置填充颜色
         context.fillStyle = grd;
     if (options.strokeStyle)
+        // 设置画笔颜色
         context.strokeStyle = options.strokeStyle;
+    // 以填充的方式绘制
     context.fill();
     context.stroke();
+    // 封闭一个图形，无瑕疵
     context.closePath();
 }
 
