a9e025d1795a9a5475c958f4b68c9e4ded229fee
justwiner <392110917@qq.com>
2019-01-26 20:54:25 +0800

    完成部分主页面组件的注释

--- src/App.js
+++ src/App.js
@@ -10,37 +10,43 @@ import './App.css';
 
 class App extends Component {
   state = {
-    borderWidth: 600,
-    spec: 14,
-    border: 30,
-    chessRecords: [],
-    gameState: {
-      ifEnd: false,
-      winner: null
+    borderWidth: 600, // 棋盘总宽度 600px
+    spec: 14, // 棋盘每行格子数目
+    border: 30, // 棋盘两侧空隙的宽度
+    chessRecords: [], // 落子位置记录列表
+    // 游戏状态
+    gameState: { 
+      ifEnd: false, // 是否结束
+      winner: null // 胜利者
     },
-    blackReady: false,
-    whiteReady: false,
-    blackObj: {},
-    whiteObj: {},
-    currentPlayer: "",
-    readyButtonShow: true,
+    blackReady: false, // 黑方是否准备
+    whiteReady: false, // 白方是否准备
+    blackObj: {}, // 黑方玩家信息
+    whiteObj: {}, // 白方玩家信息
+    currentPlayer: "", // 当前允许落子的玩家
+    readyButtonShow: true, // 是否展示准备按钮
   }
   componentDidMount () {
+    // 组件加载完成后，开始绘制棋盘
     this.drawBoard_()
   }
-  // async componentWillUpdate (nextProps, nextState) {
-  //   const {currentPlayer, blackObj, whiteObj, chessRecords, spec, gameState} = nextState
-  //   await this.ItsAIThink(currentPlayer, blackObj, whiteObj, chessRecords, spec, gameState)
-  // }
   componentDidUpdate () {
+    // 组件更新之后
     const {gameState} = this.state
+    /**
+     * 判断游戏是否结束
+     * 是 -> 显示胜利者，并取消屏幕的点击事件
+     * 否 -> 不做任何处理，继续游戏
+     */
     if (gameState.ifEnd) {
       const msg = `游戏结束,${gameState.winner === 0 ? '黑方' : '白方'}获胜!`
       message.success(msg)
+      // 取消点击事件
       this.refs.board.onclick = null
     }
   }
   ItsAIThink = async (currentPlayer, blackObj, whiteObj, chessRecords, spec, gameState) => {
+    // 只有游戏未结束的时候，才调用AI接口
     if (!gameState.ifEnd) {
       let currentPlayerObj = {}
       if (currentPlayer === "white") {
@@ -115,11 +121,20 @@ class App extends Component {
       }
     }
   }
+  /**
+   * 绘制棋盘
+   * @memberof App
+   */
   drawBoard_ () {
+    // 获取棋盘元素
     const board = this.refs.board
+    // 获取棋盘对象上下文
     const context = board.getContext('2d');
+    // 拿取棋盘基本规格信息
     const {borderWidth, border, spec, chessRecords} = this.state;
+    // 计算每格的宽度
     const width = (borderWidth - 2 * border) / spec
+    // 进行绘制
     drawBoard(context, borderWidth, border, spec, width, chessRecords)
   }
   blackOnOkFun = (obj) => {
